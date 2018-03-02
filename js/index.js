6407f210408849c14fbfe0c8ef79d7e839e7035d
pgyogesh <yogeshjadhav96@gmail.com>
2018-03-03 00:17:13 +0530

    first commit

---
+++ js/index.js
@@ -0,0 +1,258 @@
+var inputReady = true;
+var input = $('.landing-input');
+var txt = "Hi";
+var chatFlag = 0;
+input.focus();
+$('.container').on('click', function(e)
+{
+  input.focus();
+});
+
+input.on('keyup', function(e)
+{
+  $('.new-output').text(input.val());
+  // console.log(inputReady);
+});
+
+$('.landing-form').on('submit', function(e)
+{
+  e.preventDefault();
+  var val = $(this).children($('.landing-input')).val().toLowerCase();
+  var color;
+  
+  if(chatFlag == 1)
+  {
+	txt = val;
+	val = "chat";
+  }
+  
+  if(val.indexOf("color") >= 0)
+  {
+	  var temp = val.split(" ");
+	  val = temp[0];
+	  color = temp[1];
+	  setCookie("color",color,28);
+  }
+  var href;
+
+	
+	switch(val)
+	{
+		case "help":
+			showHelp();
+			break;
+		case "blog":
+			redirect_blog();
+			break;
+		case "resume":
+			redirect_cv();
+			break;
+		case "photos":
+			redirect_photography();
+			break;
+		case "cmd":
+			startCmd();
+			break;
+		case "cls":
+			clearscreen();
+			break;
+		case "reset":
+			resetCmd();
+			break;
+		case "exit":
+			redirect_global();
+			break;
+		case "":
+			newline();
+			break;
+		default:
+			resetForm();
+	}
+});
+
+function resetForm(state)
+{
+  var message = "Sorry that command is not recognized."
+  var input = $('.landing-input');
+
+  if (state){
+    $('.asciicanvas').removeClass('asciicanvas');
+    message = "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+  }
+
+  $('.new-output').removeClass('new-output');
+  input.val('');
+  $('.terminal').append('<p class="prompt">' + message + '</p><p class="prompt output new-output"></p>');
+
+  $('.new-output').velocity(
+    'scroll'
+  ), {duration: 100}
+}
+
+function showHelp()
+{
+	$('.terminal').append("<div class='prompt'>" +
+								"<table>" +
+									"<caption>Commands</caption>" +
+									"<col width='130'>" +
+									/* "<tr>" +
+										"<td>chat</td>" +
+										"<td>spawns a chatbot. type \"bye\" to stop chatting.</td>" +
+									"</tr>" + */
+			      						
+									"<tr>" +
+										"<td>exit</td>" +
+										"<td>redirects to non-geeky website.</td>" +
+									"</tr>" +
+									"<tr>" +
+										"<td>blog</td>" +
+										"<td>Redirect to my blog.</td>" +
+									"</tr>" +
+									"<tr>" +
+										"<td>photos</td>" +
+										"<td>See my photography.</td>" +
+									"</tr>" +
+									"<tr>" +
+										"<td>resume</td>" +
+										"<td>Redirect to my online resume.</td>" +
+									"</tr>" +
+									"<tr>" +
+										"<td>cmd</td>" +
+										"<td>Starts a new instance of the command prompt.</td>" +
+									"</tr>" +
+									"<tr>" +
+										"<td>cls</td>" +
+										"<td>Clears the screen.</td>" +
+									"</tr>" +
+									"<tr>" +
+										"<td>reset</td>" +
+										"<td>Resets the command prompt.</td>" +
+									"</tr>" +
+								"</table>" +
+							"</div>");	
+	newline();
+}
+
+
+function redirect_cv()
+{
+	setTimeout(function(){
+		location.href = "http://pgyogesh.com/online-cv/";
+		}, 3000);
+		
+	$('.terminal').append("<p class='prompt'>Redirecting</p>");
+		
+	setRedirectingPattern("*");
+	
+}
+
+
+function redirect_photography()
+{
+	setTimeout(function(){
+		location.href = "http://pgyogesh.com/photography/";
+		}, 3000);
+		
+	$('.terminal').append("<p class='prompt'>Redirecting</p>");
+		
+	setRedirectingPattern("*");
+	
+}
+
+
+
+function redirect_blog()
+{
+	setTimeout(function(){
+		location.href = "http://pgyogesh.com/blog/";
+		}, 3000);
+		
+	$('.terminal').append("<p class='prompt'>Redirecting</p>");
+		
+	setRedirectingPattern("*");
+	
+}
+
+
+
+function redirect_global()
+{
+	setTimeout(function(){
+		location.href = "http://pgyogesh.com/global/";
+		}, 3000);
+		
+	$('.terminal').append("<p class='prompt'>Redirecting</p>");
+		
+	setRedirectingPattern("*");
+	
+}
+
+function setRedirectingPattern(msg)
+{
+	setInterval(function(){
+		
+		$('.new-output').removeClass('new-output');
+		input.val('');
+		
+		$('.terminal').append("<p class='prompt output new-output'>"+msg+"</p>");
+		msg = msg + "*";
+		
+		$('.new-output').velocity(
+			'scroll'
+		), {duration: 100}
+		
+	},500);
+	
+}
+
+
+function startCmd()
+{
+	var redirectWindow = window.open('http://pgyogesh.com/', '_blank');
+    redirectWindow.location;
+	
+	newline();
+}
+
+function resetCmd()
+{
+	setCookie("color","",0);
+	clearscreen();
+}
+
+function clearscreen()
+{
+	location.reload();
+}
+
+
+function newline()
+{
+	$('.new-output').removeClass('new-output');
+	input.val('');
+	$('.terminal').append('<p class="prompt output new-output"></p>');
+
+	$('.new-output').velocity(
+		'scroll'
+	), {duration: 100}
+}
+
+function setCookie(cname, cvalue, exdays) 
+{
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + "; " + expires;
+}
+
+function getCookie(cname) 
+{
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1);
+        if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
+    }
+    return "";
+}
