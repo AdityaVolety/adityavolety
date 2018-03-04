d59761c40e786b31c8dc7da50408c85c6503a43e
Yogesh Jadhav <yogeshjadhav96@gmail.com>
2018-03-05 01:35:48 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -34,6 +34,7 @@ $('.landing-form').on('submit', function(e)
 	  setCookie("color",color,28);
   }
   var href;
+
 	
 	switch(val)
 	{
@@ -332,89 +333,6 @@ function clearscreen()
 {
 	location.reload();
 }
-
-	function showYogeshASCII(){
-		$('.terminal').append("<div class='asciicanvas'>"+
-        <p class='prompt'> @@@@@@@    @@@@@@@@  @@@ @@@   @@@@@@    @@@@@@@@  @@@@@@@@   @@@@@@   @@@  @@@  </p>
-		<p class='prompt'> @@@@@@@@  @@@@@@@@@  @@@ @@@  @@@@@@@@  @@@@@@@@@  @@@@@@@@  @@@@@@@   @@@  @@@  </p>
-		<p class='prompt'> @@!  @@@  !@@        @@! !@@  @@!  @@@  !@@        @@!       !@@       @@!  @@@  </p>
-		<p class='prompt'> !@!  @!@  !@!        !@! @!!  !@!  @!@  !@!        !@!       !@!       !@!  @!@  </p>
-		<p class='prompt'> @!@@!@!   !@! @!@!@   !@!@!   @!@  !@!  !@! @!@!@  @!!!:!    !!@@!!    @!@!@!@!  </p>
-		<p class='prompt'> !!@!!!    !!! !!@!!    @!!!   !@!  !!!  !!! !!@!!  !!!!!:     !!@!!!   !!!@!!!!  </p>
-		<p class='prompt'> !!:       :!!   !!:    !!:    !!:  !!!  :!!   !!:  !!:            !:!  !!:  !!!  </p>
-		<p class='prompt'> :!:       :!:   !::    :!:    :!:  !:!  :!:   !::  :!:           !:!   :!:  !:!  </p>
-		<p class='prompt'>  ::        ::: ::::     ::    ::::: ::   ::: ::::   :: ::::  :::: ::   ::   :::  </p>
-		<p class='prompt'>  :         :: :: :      :      : :  :    :: :: :   : :: ::   :: : :     :   : :  </p></div>");
-
-		
-		var lines = $('.asciicanvas p');
-		$.each(lines, function(index, line){
-			setTimeout(function(){
-				$(line).css({
-					"opacity": 1
-				});
-
-				textEffect($(line))
-			}, index * 100);
-		});
-
-		$('.new-output').velocity(
-			'scroll'
-		), {duration: 100}
-
-		setTimeout(function(){
-			/* var gif;
-
-			$.get('http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=kittens', function(result){
-				gif = result.data.image_url;
-				$('.terminal').append('<img class="kitten-gif" src="' + gif + '"">');
-				resetForm(true);
-			}); */
-			resetForm(true);
-		}, (lines.length * 100) + 1000);
-		
-		
-	}
-
-	function textEffect(line){
-		var alpha = [';', '.', ',', ':', ';', '~', '`'];
-		var animationSpeed = 10;
-		var index = 0;
-		var string = line.text();
-		var splitString = string.split("");
-		var copyString = splitString.slice(0);
-
-		var emptyString = copyString.map(function(el){
-		    return [alpha[Math.floor(Math.random() * (alpha.length))], index++];
-		})
-
-		emptyString = shuffle(emptyString);
-
-		$.each(copyString, function(i, el){
-		    var newChar = emptyString[i];
-		    toUnderscore(copyString, line, newChar);
-
-		    setTimeout(function(){
-		      fromUnderscore(copyString, splitString, newChar, line);
-		    },i * animationSpeed);
-		  })
-	}
-
-	function toUnderscore(copyString, line, newChar){
-		copyString[newChar[1]] = newChar[0];
-		line.text(copyString.join(''));
-	}
-
-	function fromUnderscore(copyString, splitString, newChar, line){
-		copyString[newChar[1]] = splitString[newChar[1]];
-		line.text(copyString.join(""));
-	}
-
-
-	function shuffle(o){
-	    for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
-	    return o;
-	};
 
 
 function newline()
